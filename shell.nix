9f8adc1aa90bc123fd36a5b18fcc18fdea7c3bd6
Peter Schueler <peter@orangemirrorentertainment.com>
2021-08-20 10:19:39 +0200

    Add Config Files
    .envrc, pyproject.toml, shell.nix, pre-commit-config.yaml, .gitignore

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> {};
+let
+	game-packages = python-packages: with python-packages; [
+		ipython
+		pytest_5
+		pillow
+		black
+		flake8
+		isort
+		django_3
+		pytest-django
+	];
+	py-data = python39.withPackages game-packages;
+in mkShell {
+	buildInputs = [
+		py-data
+		fswatch
+		pre-commit
+		fish
+		nox
+	];
+	shellHook = ''
+	export ENV_NAME=MorFgeet
+	'';
+}
