8e4f5c0282787d39818f6ccf0d7c5b1e42708278
Peter Schueler <peter@orangemirrorentertainment.com>
2021-08-23 06:51:06 +0200

    Add New Fields To Models Link, Node

--- MorFgeet/storygraph/models.py
+++ MorFgeet/storygraph/models.py
@@ -5,20 +5,34 @@ from django.db import models
 
 class DataBlock(models.Model):
     text = models.TextField()
-    link = models.ForeignKey("Link", on_delete=models.CASCADE)
+    link = models.OneToOneField(
+        "Link", on_delete=models.CASCADE, related_name="data_block"
+    )
 
     class Meta:
         verbose_name = "Data Block"
         verbose_name_plural = "Data Block"
 
 
+class NodeManager(models.Manager):
+    def get_initial(self) -> "Node":
+        return Node.objects.get(position=0)
+
+
 class Node(models.Model):
+    objects = NodeManager()
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    position = models.IntegerField(default=0)
+    position = models.IntegerField(default=0, unique=True)
+    title = models.CharField(max_length=250, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name = "Node"
         verbose_name_plural = "Nodes"
+
+    def add_link(self, link: "Link"):
+        self.links.add(link)
 
 
 class Link(models.Model):
@@ -32,3 +46,7 @@ class Link(models.Model):
     class Meta:
         verbose_name = "Link"
         verbose_name_plural = "Links"
+
+    @property
+    def text(self):
+        return self.data_block.text
